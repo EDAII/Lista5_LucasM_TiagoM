cb19106c9de0e08ffc1f97d414ae4fa14c9e2c31
Tiago Miguel <tmcstiago@gmail.com>
2019-06-02 23:02:23 -0300

    Arvore Red Black

---
+++ leitura.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    vector<int> insercoes;
+    vector<int> buscas;
+    vector<int> remocoes;
+
+    ifstream arquivo1("./instrocoes.txt");
+
+    while()
+
+    return 0;
+}
